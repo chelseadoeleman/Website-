b6a3e6aebc0034ab1308663132020610f8c362d0
chelseadoeleman <35819277+chelseadoeleman@users.noreply.github.com>
2018-01-30 04:55:13 +0100

    Revert "Initial commit"
    This reverts commit 7ee2670a2ac530451fa096f6e709e19e6a26c018.

--- script/filter.js
+++
@@ -1,9 +0,0 @@
-/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
-var knop = document.querySelector("li:nth-child(4)");
-console.log(knop);
-var filters = document.querySelector(".filter > .filterOpties");
-console.log(filters);
-
-knop.addEventListener('click', function () {
-    filters.classList.toggle("filterOpties2");
-});
--- script/gedrag.js
+++
@@ -1,9 +0,0 @@
-/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
-var button = document.getElementById("zoekenHeader");
-//console.log(button);
-var zoekveld = document.querySelector("div > .zoeken");
-//console.log(zoekveld);
-
-button.addEventListener('click', function () {
-    zoekveld.classList.toggle("zoekenVisible");
-});
